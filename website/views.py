6388a70bfc976e3dba17055cab4cc1c4d3e18e72
Sashank Ravipati <sashankr04@gmail.com>
2023-06-03 18:44:48 +0530

    added web generation functionality

--- website/views.py
+++ website/views.py
@@ -21,8 +21,8 @@ def home():
 @views.route('/ffcsgen')
 @login_required
 def ffcsgen():
-    global df1 
-    courses12=df1['Course'].unique()
+    global df 
+    courses12=df['Course'].unique()
     courses1=courses12[:-1]
     return render_template("ffcsgen.html",user=current_user,courses1=courses1)
 
@@ -44,7 +44,29 @@ def time():
 @views.route('/morning',methods=['POST'])
 @login_required
 def morning():
+    global df1,selected_choices,df_sorted,df,i
+    df1=df1[df1['Slot'].str.endswith('1')]
+    df1=df1.reset_index(drop=True)
+    df_sorted = df1.sort_values(by='Rating', ascending=False).reset_index(drop=True)
+    df_sorted = df_sorted[df_sorted['Course'].isin(selected_choices)]
+    df_sorted=df_sorted.reset_index(drop=True)
+    x=generatett()
 
 
+    return render_template("morning.html",user=current_user,x=x)
+def generatett():
+    global selected1, df_sorted, selected_choices, df, i
+    sellen=len(selected_choices)
+    i+=1
+    if i==25:
+        return
+    else:    
+        selected1 = pd.DataFrame()        
+        while (selected_choices):
+            first_entry = df_sorted.iloc[0]
+            selected1 = pd.concat([selected1, pd.DataFrame([first_entry])], ignore_index=True)
+            df_sorted = df_sorted[(df_sorted['Course'] != first_entry['Course']) & (df_sorted['Slot'] != first_entry['Slot'])].reset_index(drop=True)
+            selected_choices = df_sorted['Course'].unique().tolist()
 
-    return render_template("morning.html",user=current_user,selected_choices=selected_choices)
+        if (len(selected1)==sellen):
+            return selected1
