b3cad3b3a5e05698fc6a0c89fbe69d5428933d9f
pk-vishnu <vishnuvpk05@gmail.com>
2023-06-03 14:13:12 +0530

    web update

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint,render_template,session
+from flask import Flask,Blueprint,render_template,session,request
 from flask_login import login_required, current_user
+
 views = Blueprint('views', __name__)
 
 @views.route('/')
@@ -9,5 +10,16 @@ def home():
 
 
 @views.route('/ffcsgen')
+@login_required
 def ffcsgen():
-    return render_template("ffcsgen.html",user=current_user)
+    import pandas as pd
+    df = pd.read_csv('ffcs.csv')
+    df1=pd.read_csv('ffcs.csv')
+    courses1=df1['Course'].unique()
+    return render_template("ffcsgen.html",user=current_user,courses1=courses1)
+
+@views.route('/process_form', methods=['POST'])
+@login_required
+def process_form():
+    selected_choices = request.form.getlist('selected_choices')
+    return f"Selected Choices: {selected_choices}"
